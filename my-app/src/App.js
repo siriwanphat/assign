065f788113ccdb3d282ef8074a0064608e8a06ba
oeng <siriwan.sp50@gmail.com>
2019-10-27 21:27:47 +0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import { Button } from 'reactstrap';
+import Delivery from './delivery';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Delivery/>
+    </div>
+  );
+}
+
+export default App;
